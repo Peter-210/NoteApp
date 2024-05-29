2cc1b3e0c1ea21dfa8fa7260ea0a275b5920e015
Peter-012 <151083213+Peter-012@users.noreply.github.com>
2024-05-29 05:59:04 -0400

    Manual Binding Context for mainpage
    The manual binding context script (cs) can update the new binding context after editing the note.

--- NoteApp/Pages/MainPage.xaml.cs
+++ NoteApp/Pages/MainPage.xaml.cs
@@ -1,9 +1,25 @@
+using Microsoft.Maui.Controls;
+using NoteApp.ViewModels;
+
 namespace NoteApp.Pages;
 
 public partial class MainPage : ContentPage
 {
+	public NoteViewModel NoteViewModel { get; private set; }
+
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+
+        NoteViewModel = new NoteViewModel();
+		BindingContext = NoteViewModel;
+
+        noteList.BindingContext = NoteViewModel;
+        newNoteButton.BindingContext = NoteViewModel;
+    }
 }
